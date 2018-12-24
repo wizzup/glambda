c5bc8e94136d9b5f59d8471da6f009a6b6c049b7
wisut hantanong <wizzup@gmail.com>
2018-12-24 15:38:15 +0700

    done: Ex1

--- exercises/Ex1.hs
+++ exercises/Ex1.hs
@@ -10,9 +10,17 @@ data STy ty where
   SIntTy   :: STy Int
   SBoolTy  :: STy Bool
   SMaybeTy :: STy a -> STy (Maybe a)
+  SListTy  :: STy a -> STy [a]
+  SWrapTy  :: STy a -> STy (Wrap a)
+  SUnitTy  :: STy ()
+  SFuncTy  :: STy a -> STy b -> STy ((->) a b)
 
 -- | Produce a "zero" of that type
 zero :: STy ty -> ty
-zero SIntTy       = 0
-zero SBoolTy      = False
-zero (SMaybeTy _) = Nothing
+zero SIntTy         = 0
+zero SBoolTy        = False
+zero (SMaybeTy _)   = Nothing
+zero (SListTy _)    = []
+zero (SWrapTy sa)   = Wrap $ zero sa
+zero SUnitTy        = ()
+zero (SFuncTy _ sb) = const $ zero sb
